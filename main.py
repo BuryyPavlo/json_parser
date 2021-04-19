6810777d1c1f09f4dd5f07b4e17a32e149420754
Pavlo <BuryyPavlo@users.noreply.github.com>
2021-04-19 05:33:55 -0400

    added logic for skip None

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import json
 
-df = pd.read_csv('data1.csv', skiprows=range(0, 3))
+df = pd.read_csv('data.csv', skiprows=range(0, 3))
 
 old_headers = df.columns.values.tolist()
-df1 = df.where(df.notnull(), None)
+df_new = df.where(df.notnull(), None)
 
 new_headers = []
 for header in old_headers:
@@ -25,14 +25,15 @@ for row in reader:
         if key in ['ItemId', 'Category ID', 'Custom Label (SKU)', 'Title', 'Format','VariationSet','SiteID','Reserved1-Do not edit or delete']:
             new_row[key] = row[key]
         else:
-            new_aspert = {}
-            new_aspert['LocalizedAspectName'] = key
-            if isinstance(row[key], str) and ',' in row[key] and row[key]:
-                value = row[key].split(',')
-                new_aspert['AspectValues'] = value
-            else:
-                new_aspert['AspectValues'] = row[key]
-            asperts.append(new_aspert)
+            if row[key] is not None:
+                new_aspert = {}
+                new_aspert['LocalizedAspectName'] = key
+                if isinstance(row[key], str) and ',' in row[key]:
+                    value = row[key].split(',')
+                    new_aspert['AspectValues'] = value
+                else:
+                    new_aspert['AspectValues'] = row[key]
+                asperts.append(new_aspert)
     new_row['Aspects'] = asperts
     results.append(new_row)
 
