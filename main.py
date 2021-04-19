c131cd5b03d211ac3f57d135b0e4a9f1c38ff620
Pavlo Buryi <pavlo.buryi@b6tp.com>
2021-04-18 20:57:06 -0400

    Create main.py
    done

---
+++ main.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import json
+
+df = pd.read_csv('data1.csv', skiprows=range(0, 3))
+
+old_headers = df.columns.values.tolist()
+df1 = df.where(df.notnull(), None)
+
+new_headers = []
+for header in old_headers:
+    if "C:" in header:
+        new_headers.append(header.replace("C:", ""))
+    else:
+        new_headers.append(header)
+
+df_new.columns = new_headers
+
+reader = df_new.to_dict('records')
+
+results = []
+for row in reader:
+    new_row = {}
+    asperts = []
+    for key in row:
+        if key in ['ItemId', 'Category ID', 'Custom Label (SKU)', 'Title', 'Format','VariationSet','SiteID','Reserved1-Do not edit or delete']:
+            new_row[key] = row[key]
+        else:
+            new_aspert = {}
+            new_aspert['LocalizedAspectName'] = key
+            if isinstance(row[key], str) and ',' in row[key] and row[key]:
+                value = row[key].split(',')
+                new_aspert['AspectValues'] = value
+            else:
+                new_aspert['AspectValues'] = row[key]
+            asperts.append(new_aspert)
+    new_row['Aspects'] = asperts
+    results.append(new_row)
+
+with open('results.json', 'w') as outfile:
+    json.dump(results, outfile)
